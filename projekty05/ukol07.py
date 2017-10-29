7875f5b65fe849ba464da5ff5d653fd3bee34c95
evawashere <32848244+evawashere@users.noreply.github.com>
2017-10-29 10:45:34 +0100

    Create ukol07.py

---
+++ projekty05/ukol07.py
@@ -0,0 +1,13 @@
+#pocet vyskytu "k" v textu
+
+song="""Skákal pes přes oves
+přes zelenou louku
+šel za ním myslivec
+péro na klobouku.
+
+Pejsku náš, co děláš
+žes tak vesel stále?
+Řek bych vám, nevím sám
+hop, a skákal dále."""
+
+print(song.count("k"))
